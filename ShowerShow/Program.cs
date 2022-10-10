99ee6e3dac1139de0c46dbc3815081774950bb7a
egecnr <54850459+egecnr@users.noreply.github.com>
2022-10-10 08:27:21 +0200

    Cleaned the classes that arent used

--- ShowerShow/Program.cs
+++ ShowerShow/Program.cs
@@ -17,26 +17,6 @@ namespace ShowerShow
 
         static async Task Main(string[] args)
         {
-            //for db test
-            using (var ct = new DatabaseContext())
-            {
-
-                User u1 = new();
-                u1.Id = Guid.NewGuid();
-                u1.Username = "Todd";
-                u1.Friends = new List<UserFriend>();
-                UserFriend friend = new UserFriend();
-                friend.Id = Guid.NewGuid();
-                friend.Username = "Toddsfriend";
-                u1.Friends.Add(friend);
-                u1.Achievements = new List<Achievement> { new Achievement() };
-                u1.UserBadge = "popeye";
-                u1.Email = "toddsboringemailaddress";
-                u1.Name = "Todd toddler";
-                u1.PasswordHash = "Youcantguessthisrightbro";
-                ct.Users?.Add(u1);
-                await ct.SaveChangesAsync();
-            }
             var host = new HostBuilder()
                     .ConfigureFunctionsWorkerDefaults()
                     .ConfigureOpenApi()
