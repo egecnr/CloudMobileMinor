de1d52a6850950308f7cc1e250aa0b97916daf0d
egecnr <54850459+egecnr@users.noreply.github.com>
2022-10-20 13:08:02 +0200

    will be back here

--- ShowerShow/Model/UserFriend.cs
+++ ShowerShow/Model/UserFriend.cs
@@ -7,13 +7,24 @@ namespace ShowerShow.Model
 {
     public class UserFriend
     {
+        [JsonRequired]
         public Guid Id { get; set; }
+
+        [JsonRequired]
+        public Guid MainUserId { get; set; }
+
+    
+
         [JsonRequired]
         public string UserName { get; set; }
+
+        [JsonRequired]
+        public bool IsUserFavorite { get; set; }
+
         public UserFriend() { }
         public UserFriend(Guid id,string userName)
         {
-            Id = id;
+            MainUserId = id;
             UserName = userName;
         }
     }
