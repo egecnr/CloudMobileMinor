4f7fa139c0ca0d52ad0885c6d4fd605d52deca03
egecnr <54850459+egecnr@users.noreply.github.com>
2022-11-04 06:42:57 +0100

    All tests are committed and works.

--- ShowerShow/Service/UserPreferencesService.cs
+++ ShowerShow/Service/UserPreferencesService.cs
@@ -6,6 +6,7 @@ using ShowerShow.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,10 +15,12 @@ namespace ShowerShow.Service
     public class UserPreferencesService : IUserPrefencesService
     {
         private IUserPreferencesRepository _userPreferencesRepository;
+        private IUserService userService;
 
-        public UserPreferencesService(IUserPreferencesRepository userPreferencesRepository)
+        public UserPreferencesService(IUserPreferencesRepository userPreferencesRepository, IUserService userService)
         {
             this._userPreferencesRepository = userPreferencesRepository;
+            this.userService = userService;
         }
         public async Task CreateUserPreferences(Guid userId)
         {
@@ -28,12 +31,27 @@ namespace ShowerShow.Service
 
         public async Task<PreferencesDTO> GetUserPreferencesById(Guid userId)
         {
-            return await _userPreferencesRepository.GetUserPreferenceById(userId);
+            if (await userService.CheckIfUserExistAndActive(userId))
+            {
+               return await _userPreferencesRepository.GetUserPreferenceById(userId);
+              
+            }
+            else
+            {
+                throw new Exception("User doesn't exist");
+            }
         }
 
         public async Task UpdatePreferenceById(Guid userId, PreferencesDTO updatePreferencesDTO)
         {
-            await _userPreferencesRepository.UpdatePreferenceById(userId, updatePreferencesDTO);
+            if (await userService.CheckIfUserExistAndActive(userId))
+            {
+                await _userPreferencesRepository.UpdatePreferenceById(userId, updatePreferencesDTO);
+            }
+            else
+            {
+                throw new Exception("User doesn't exist");
+            }
         }
     }
 }
